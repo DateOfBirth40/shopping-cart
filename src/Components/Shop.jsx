9e915049473744b49b361ae7cf5eba2e8176294f
Derek O'Brien <derekobrien40@gmail.com>
2023-06-19 18:31:29 -0400

    Render array of shoes

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Card from './Card';
+import shoes from '../shoes';
+
+const shoeItems = shoes.map((shoe, index) =>
+  <Card
+   key={index}
+   model={shoe.model}
+   color={shoe.color}
+   price={shoe.price}
+   image={shoe.image} />
+  );
 
 const Shop = () => {
   return (
     <div>
       <Navbar />
-      <h1>Hello from Shop</h1>
+      {shoeItems}
     </div>
   );
 };
