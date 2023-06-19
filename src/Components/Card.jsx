827b9012a085d34f1033e5f50979f23c23ae002a
Derek O'Brien <derekobrien40@gmail.com>
2023-06-19 18:30:46 -0400

    Update component to receive props

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import shoes from '../shoes';
 import '../styles/Card.css';
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className='card'>
-      <p className='model'>{shoes[0].model}</p>
+      <p className='model'>{props.model}</p>
       <div className='color-price'>
-        <p className='color'>{`'${shoes[0].color}'`}</p>
-        <p className='price'>{`$${shoes[0].price}`}</p>
+        <p className='color'>{`'${props.color}'`}</p>
+        <p className='price'>{`$${props.price}`}</p>
       </div>
       <div className='image-div'>
-        <img src={shoes[0].image} className='image' />
+        <img src={props.image} className='image' />
       </div>
     </div>
   )
