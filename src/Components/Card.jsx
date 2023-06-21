7bc76195f724f728a8ab182b99ec28b008cfef8b
Derek O'Brien <derekobrien40@gmail.com>
2023-06-21 16:34:23 -0400

    Create select options

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,6 +3,15 @@ import shoes from '../shoes';
 import '../styles/Card.css';
 
 export default function Card(props) {
+  // Create a state that controls the size of the shoe and adding it to cart
+  // Maybe create an object that contains size, price, and boolean if it is in cart or not
+  const [size, setSize] = React.useState('');
+
+  const handleSize = (e) => {
+    setSize(e.target.value);
+  }
+  console.log(size);
+
   return (
     <div className='card'>
       <p className='model'>{props.model}</p>
@@ -12,6 +21,34 @@ export default function Card(props) {
       </div>
       <div className='image-div'>
         <img src={props.image} className='image' />
+      </div>
+      <div className='card--buttons'>
+        <select onChange={handleSize} className='card--select'>
+          <option value='' selected disabled>Size</option>
+          <option value='3.5' key='3.5'>3.5</option>
+          <option value='4' key='4'>4</option>
+          <option value='4.5' key='4.5'>4.5</option>
+          <option value='5' key='5'>5</option>
+          <option value='5.5' key='5.5'>5.5</option>
+          <option value='6' key='6'>6</option>
+          <option value='6.5' key='6.5'>6.5</option>
+          <option value='7' key='7'>7</option>
+          <option value='7.5' key='7.5'>7.5</option>
+          <option value='8' key='8'>8</option>
+          <option value='8.5' key='8.5'>8.5</option>
+          <option value='9' key='9'>9</option>
+          <option value='9.5' key='9.5'>9.5</option>
+          <option value='10' key='10'>10</option>
+          <option value='10.5' key='10.5'>10.5</option>
+          <option value='11' key='11'>11</option>
+          <option value='11.5' key='11.5'>11.5</option>
+          <option value='12' key='12'>12</option>
+          <option value='12.5' key='12.5'>12.5</option>
+          <option value='13' key='13'>13</option>
+          <option value='14' key='14'>14</option>
+          <option value='15' key='15'>15</option>
+        </select>
+        <button className='add-to-cart'>Add to Cart</button>
       </div>
     </div>
   )
