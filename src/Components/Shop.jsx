83d07c52325d5692f4c0782768d1ce10bee46b36
Derek O'Brien <derekobrien40@gmail.com>
2023-07-05 15:06:35 -0400

    addToCart works; shoes is now mapped in return statement

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import Navbar from './Navbar';
 import Card from './Card';
 import shoes from '../shoes';
 import '../styles/Shop.css';
 
-const shoeItems = shoes.map((shoe, index) =>
-  <Card
-   key={index}
-   model={shoe.model}
-   color={shoe.color}
-   price={shoe.price}
-   image={shoe.image} />
-  );
-
 const Shop = () => {
-  const [cart, setCart] = React.useState('');
+  const [cart, setCart] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+
+  function addToCart(item) {
+    setCart(prevCart => [...prevCart, {model: item.model, size: item.size}]);
+    setTotal(current => current + item.price);
+    // console.log(item.name);
+  }
+
+  console.log('Cart: ', cart);
+  console.log(total);
+
   return (
     <div>
-      <Navbar />
       <div className='card-grid'>
-        {shoeItems}
+        {shoes.map((shoe, index) =>
+          <Card
+            key={index}
+            model={shoe.model}
+            color={shoe.color}
+            price={shoe.price}
+            image={shoe.image}
+            cart={cart}
+            total={total}
+            addToCart={addToCart} />
+          )}
       </div>
     </div>
   );
