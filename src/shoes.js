c5e4676e7e05ef9486e9f298c47e9e35883dbe73
Derek O'Brien <derekobrien40@gmail.com>
2023-07-12 11:03:33 -0400

    Add id to objects

--- src/shoes.js
+++ src/shoes.js
@@ -17,96 +17,112 @@ import nb9060raincloud from './assets/nb9060-raincloud.webp';
 
 const shoes = [
   {
+    id: 0,
     model: 'A Ma Maniére x Air Jordan 1 High OG',
     color: 'Airness',
     image: aj1ama,
     price: 220
   },
   {
+    id: 1,
     model: 'Air Jordan 1 Retro High OG',
     color: 'Bred Toe',
     image: aj1bred,
     price: 220
   },
   {
+    id: 2,
     model: 'Air Jordan 1 Retro High OG',
     color: 'Chicago Lost & Found',
     image: aj1chicago,
     price: 220
   },
   {
+    id: 3,
     model: 'Air Jordan 1 Retro High OG',
     color: 'Shadow 2.0',
     image: aj1shadow2,
     price: 220
   },
   {
+    id: 4,
     model: 'Air Jordan 1 Retro High OG',
     color: 'University Blue',
     image: aj1unc,
     price: 220
   },
   {
+    id: 5,
     model: 'Travis Scott x Air Jordan 1 Low OG SP',
     color: 'Black Phantom',
     image: aj1lblack,
     price: 150
   },
   {
+    id: 6,
     model: 'Air Jordan 1 Low OG',
     color: 'Chicago',
     image: aj1lchicago,
     price: 130
   },
   {
+    id: 7,
     model: 'Air Jordan 1 Low OG',
     color: 'Neutral Grey',
     image: aj1lgrey,
     price: 130
   },
   {
+    id: 8,
     model: 'Travis Scott x Air Jordan 1 Low OG SP',
     color: 'Reverse Mocha',
     image: aj1lrevmocha,
     price: 150
   },
   {
+    id: 9,
     model: 'Air Jordan 1 Low OG',
     color: 'Starfish',
     image: aj1lstarfish,
     price: 130
   },
   {
+    id: 10,
     model: 'Air Jordan 1 Low OG',
     color: 'University Blue',
     image: aj1lunc,
     price: 130
   },
   {
+    id: 11,
     model: 'NB 530',
     color: 'White Natural Indigo',
     image: nb530indigo,
     price: 150
   },
   {
+    id: 12,
     model: 'NB 550',
     color: 'White Grey',
     image: nb550white,
     price: 150
   },
   {
+    id: 13,
     model: 'Teddy Santis x NB 990v3 Made in USA',
     color: 'Sea Salt',
     image: nb990v3seasalt,
     price: 150
   },
   {
+    id: 14,
     model: 'NB 2002R',
     color: 'Calm Taupe',
     image: nb2002rtaupe,
     price: 150
   },
   {
+    id: 15,
     model: 'NB 9060',
     color: 'Rain Cloud',
     image: nb9060raincloud,
